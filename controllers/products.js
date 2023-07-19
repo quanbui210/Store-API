0a8a21ecf579719b4c66c7daf321642a541dc8eb
Quan Bui <quanbui021001@gmail.com>
2023-07-19 12:21:47 +0300

    sorting, query params...

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,38 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({})
+    const products = await Product.find({}).select('name price')
     res.status(200).json(products)
 }
 
 const getAllProducts = async(req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const { featured, company, name, sort, fields } = req.query
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+    let result = Product.find(queryObject)
+
+    //sort
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    const products = await result
+    res.status(200).json(products)
 }
 
 module.exports = {
